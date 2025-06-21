fdf2ee20beed1a152b21ef1f45b0fac0810910ef
CanulJ <jesus34canul@gmail.comm>
2025-06-21 16:34:16 -0600

    Cambio de volver

--- src/app/Pages/conocenos/conocenos.component.ts
+++ src/app/Pages/conocenos/conocenos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-conocenos',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './conocenos.component.css'
 })
 export class ConocenosComponent {
+
+  constructor(private router: Router) {}
+
+ Inicio(): void {
+    this.router.navigate(['/inicio']);
+  }
 
 }
